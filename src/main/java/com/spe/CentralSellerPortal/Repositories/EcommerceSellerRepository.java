7455337f874f8666e24378b2b97276649e7248a0
eshi13 <eshita.sharma13@gmail.com>
2021-03-01 12:49:46 +0530

    first commit

--- src/main/java/com/spe/CentralSellerPortal/Repositories/EcommerceSellerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spe.CentralSellerPortal.Repositories;
-
-import com.spe.CentralSellerPortal.Entities.Ecommerce_seller;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EcommerceSellerRepository extends JpaRepository<Ecommerce_seller, Long>  {
-}
